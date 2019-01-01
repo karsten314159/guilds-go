6eb4f8b60fb42ed0a9cf898fe83d6ff569bdcaa2
Karsten <karsten@snapcardster.com>
2019-01-01 20:52:52 +0100

    build

---
+++ build/precache-manifest.638791e821212adbb41ed8623882b801.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32563b860daf5a61e94b",
+    "url": "/static/js/main.32563b86.chunk.js"
+  },
+  {
+    "revision": "608af88770c4cf0da134",
+    "url": "/static/js/1.608af887.chunk.js"
+  },
+  {
+    "revision": "32563b860daf5a61e94b",
+    "url": "/static/css/main.83f44aa4.chunk.css"
+  },
+  {
+    "revision": "2c6be07bdb5c60ec22a1de711b0b3dda",
+    "url": "/index.html"
+  }
+];
