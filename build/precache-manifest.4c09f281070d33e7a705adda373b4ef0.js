7343d96727b5ec7ddfae823e00ed212843db2cbd
Karsten <karsten@snapcardster.com>
2019-01-09 01:22:16 +0100

    build

---
+++ build/precache-manifest.4c09f281070d33e7a705adda373b4ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f505575ee576e0e6f4a",
+    "url": "/static/js/main.8f505575.chunk.js"
+  },
+  {
+    "revision": "608af88770c4cf0da134",
+    "url": "/static/js/1.608af887.chunk.js"
+  },
+  {
+    "revision": "8f505575ee576e0e6f4a",
+    "url": "/static/css/main.55dd1458.chunk.css"
+  },
+  {
+    "revision": "882c58e2e4cec8527466b8c7cf4525f7",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.638791e821212adbb41ed8623882b801.js"
+  "/precache-manifest.4c09f281070d33e7a705adda373b4ef0.js"
 );
 
 workbox.clientsClaim();
